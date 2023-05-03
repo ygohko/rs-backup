ac93f166495772f757c97e14d9ec8ab025c14c0f
Yasuaki Gohko <ygohko@gmail.com>
2023-05-03 21:17:59 +0900

    Added removing files that is not in the source directory.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::cmp::Ordering;
 use std::collections::VecDeque;
 use std::env;
 use std::fs;
-use std::time::SystemTime;
+// use std::time::SystemTime;
 use std::path::Path;
 use std::path::PathBuf;
 
@@ -34,18 +34,42 @@ fn execute(source_path: String, destination_path: String) -> std::io::Result<()>
         directory_path_buf.push(destination_path.clone());
         directory_path_buf.push(entries.path);
         if !directory_path_buf.as_path().exists() {
+            // Create destination directory
 
             // println!("path: {}", directory_path_buf.to_str().unwrap());
 
             std::fs::create_dir_all(directory_path_buf.as_path())?;
         }
+        else {
+            // Remove files that is not in the source directory
+            let destination_entries = get_directory_entries(directory_path_buf.to_str().unwrap().to_string())?;
+            for a_file_path in destination_entries.file_paths {
+                let mut path = a_file_path.clone();
+                path.replace_range(0..destination_path.len() + 1, "");
+                let mut found = false;
+                for b_file_path in entries.file_paths.clone() {
+                    if b_file_path == path {
+                        found = true;
+                        break;
+                    }
+                }
+
+                if !found {
+
+                    println!("Removing...");
+
+                    std::fs::remove_file(Path::new(&a_file_path))?;
+                }
+            }
+        }
 
         for file_path in entries.file_paths {
+            // Copy the file
             let mut file_destination_path_buf = PathBuf::new();
             file_destination_path_buf.push(destination_path.clone());
             file_destination_path_buf.push(file_path.clone());
 
-            // println!("Copying from {} to {}...", file_path.clone(), file_destination_path_buf.to_str().unwrap().to_string());
+            println!("Copying from {} to {}...", file_path.clone(), file_destination_path_buf.to_str().unwrap().to_string());
 
             copy(file_path, file_destination_path_buf.to_str().unwrap().to_string())?;
 
@@ -56,34 +80,41 @@ fn execute(source_path: String, destination_path: String) -> std::io::Result<()>
         }
     }
 
-    Ok(())
+    return Ok(());
 }
 
 fn copy(source_path: String, destination_path: String) -> std::io::Result<()> {
     let mut needed = true;
     if Path::new(&destination_path).exists() {
-	let source_metadata = Path::new(&source_path).metadata()?;
-	let source_modified = source_metadata.modified()?;
-	let destination_metadata = Path::new(&destination_path).metadata()?;
-	let destination_modified = destination_metadata.modified()?;
+        let source_metadata = Path::new(&source_path).metadata()?;
+        let source_modified = source_metadata.modified()?;
+        let destination_metadata = Path::new(&destination_path).metadata()?;
+        let destination_modified = destination_metadata.modified()?;
 
-	let result = source_modified.duration_since(SystemTime::UNIX_EPOCH);
-	let source_secs = result.ok().unwrap().as_secs();
-	let result = destination_modified.duration_since(SystemTime::UNIX_EPOCH);
-	let destination_secs = result.ok().unwrap().as_secs();
-	// println!("source: {}, destination: {}", source_secs, destination_secs);
+        /*
+        let result = source_modified.duration_since(SystemTime::UNIX_EPOCH);
+        let source_secs = result.ok().unwrap().as_secs();
+        let result = destination_modified.duration_since(SystemTime::UNIX_EPOCH);
+        let destination_secs = result.ok().unwrap().as_secs();
+        */
+        // println!("source: {}, destination: {}", source_secs, destination_secs);
 
-	if source_modified.cmp(&destination_modified) != Ordering::Greater {
-	    needed = false;
-	}
+        if source_modified.cmp(&destination_modified) != Ordering::Greater {
+            needed = false;
+        }
     }
     if needed {
 
-	// println!("Copying...");
+        println!("Copying...");
 
-	std::fs::copy(Path::new(&source_path), Path::new(&destination_path))?;
-	let source_metadata = Path::new(&source_path).metadata()?;
-	set_file_mtime(destination_path, FileTime::from_last_modification_time(&source_metadata))?;
+        std::fs::copy(Path::new(&source_path), Path::new(&destination_path))?;
+        let source_metadata = Path::new(&source_path).metadata()?;
+        set_file_mtime(destination_path, FileTime::from_last_modification_time(&source_metadata))?;
+    }
+    else {
+
+        println!("Skipping...");
+
     }
 
     return Ok(());
@@ -109,7 +140,8 @@ fn get_directory_entries(path: String) -> Result<DirectoryEntries, std::io::Erro
             }
         }
     }
-    Ok(entries)
+
+    return Ok(entries);
 }
 
 fn main() -> std::io::Result<()> {
